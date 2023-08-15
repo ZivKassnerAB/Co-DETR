44348d81f42b239ab3d607915eac19e1ca2a3054
server4 <ubuntu@Server3>
2023-08-15 22:30:09 +0300

    trying to use multi GPU's

--- sahi_infer.py
+++ sahi_infer.py
@@ -14,7 +14,10 @@ import pickle
 import pandas as pd
 import argparse
 import glob
-
+import shutil
+import torch.multiprocessing as mp
+import numpy as np
+import gdown
 torch.cuda.empty_cache()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -23,8 +26,8 @@ class Co_DETR_Sahi:
     def __init__(self, 
                  images_dir: str=None,
                  output_dir: str=None,
-                 config_path: str='./configs/Co-DETR/co_dino/co_dino_5scale_lsj_swin_large_3x_coco.py',
-                 weights_path: str='./weights/Co-DETR/co_dino_5scale_lsj_swin_large_3x_coco.pth',
+                 config_path: str=None,
+                 weights_path: str=None,
                  conf_thresh: float=0.1,
                  image_size: int=1920,
                  device: str=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
@@ -93,31 +96,21 @@ class Co_DETR_Sahi:
         self.df = pd.DataFrame(data, columns=['name', 'x_center', 'y_center', 'width', 'height', 'label', 'score'])
         self.df.to_csv(f"{self.output_dir}/co_detr.tsv", sep='\t',index=False)
         
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Co-DETR Sahi Object Detection")
+def run_on_single_gpu_with_results(device, image_subdir, args, results):
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(device.index)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
-    parser.add_argument("--images_dir", type=str, required=True, 
-                        default='/home/ubuntu/workspace/datasets/ab_car_validation_germany/2w_misses', help="Directory containing images")
-    parser.add_argument("--output_dir", type=str, required=True, 
-                        default= '/home/ubuntu/workspace/results/ab_car_validation_germany/riders/Co-DETR/2w_misses_codetr', help="Output directory")
-    parser.add_argument("--config_path", type=str, default='./configs/Co-DETR/co_dino/co_dino_5scale_lsj_swin_large_3x_coco.py', help="Path to config file")
-    parser.add_argument("--weights_path", type=str, default='./weights/Co-DETR/co_dino_5scale_lsj_swin_large_3x_coco.pth', help="Path to weights file")
-    parser.add_argument("--conf_thresh", type=float, default=0.1, help="Confidence threshold")
-    parser.add_argument("--image_size", type=int, default=3840, help="Image size")
-    parser.add_argument("--slice_height", type=int, default=1280, help="Slice height")
-    parser.add_argument("--slice_width", type=int, default=1280, help="Slice width")
-    parser.add_argument("--overlap_height_ratio", type=float, default=0.1, help="Overlap height ratio")
-    parser.add_argument("--overlap_width_ratio", type=float, default=0.1, help="Overlap width ratio")
-    
-    args = parser.parse_args()
+    chunk_output_dir = os.path.join(args.output_dir, f'gpu_{device.index}')
+    os.makedirs(chunk_output_dir, exist_ok=True)
 
     co_detr_sahi = Co_DETR_Sahi(
-        images_dir=args.images_dir,
-        output_dir=args.output_dir,
+        images_dir=image_subdir,
+        output_dir=chunk_output_dir,
         config_path=args.config_path,
         weights_path=args.weights_path,
         conf_thresh=args.conf_thresh,
-        image_size=args.image_size
+        image_size=args.image_size,
+        device=device
     )
     
     co_detr_sahi.slice_infer(
@@ -128,3 +121,89 @@ if __name__ == "__main__":
     )
     
     co_detr_sahi.parse_pickles_to_dataframe()
+    
+    results[device.index] = co_detr_sahi.df
+
+def divide_images_into_subdirs(images_dir, num_gpus):
+    subdirs = []
+    images = [f for f in os.listdir(images_dir) if os.path.isfile(os.path.join(images_dir, f))]
+    num_images_per_gpu = len(images) // num_gpus
+    
+    for i in range(num_gpus):
+        subdir_path = os.path.join(images_dir, f"gpu_{i}")
+        os.makedirs(subdir_path, exist_ok=True)
+        
+        for img_file in images[i*num_images_per_gpu: (i+1)*num_images_per_gpu]:
+            shutil.copy(os.path.join(images_dir, img_file), subdir_path)
+            
+        leftover_images = images[num_gpus*num_images_per_gpu:]
+        if leftover_images:
+            for idx, img_file in enumerate(leftover_images):
+                subdir_path = os.path.join(images_dir, f"gpu_{idx}")
+                shutil.copy(os.path.join(images_dir, img_file), subdir_path)
+        
+        subdirs.append(subdir_path)
+    
+    return subdirs
+
+def aggregate_results(output_dir, num_gpus):
+    all_dataframes = []
+
+    for gpu_index in range(num_gpus):
+        gpu_output_dir = os.path.join(output_dir, f'gpu_{gpu_index}')
+        tsv_path = os.path.join(gpu_output_dir, 'co_detr.tsv')
+        df = pd.read_csv(tsv_path, sep='\t')
+        all_dataframes.append(df)
+
+    final_df = pd.concat(all_dataframes, ignore_index=True)
+    final_df.to_csv(os.path.join(output_dir, 'co_detr.tsv'), sep='\t', index=False)
+
+def distribute_prediction_on_gpus(args):
+    devices = [torch.device(f"cuda:{i}") for i in range(torch.cuda.device_count())]
+    subdirs = divide_images_into_subdirs(args.images_dir, len(devices))
+    
+    results = mp.Manager().dict()
+
+    with mp.Pool(processes=len(devices)) as pool:
+        chunk_args = [(devices[i], subdirs[i], args, results) for i in range(len(devices))]
+        pool.starmap(run_on_single_gpu_with_results, chunk_args)
+
+    aggregate_results(args.output_dir, len(devices))
+    
+    cleanup_subdirs(args.images_dir, len(devices))
+    
+def cleanup_subdirs(images_dir, num_gpus):
+    for i in range(num_gpus):
+        subdir_path = os.path.join(images_dir, f"gpu_{i}")
+        if os.path.exists(subdir_path):
+            shutil.rmtree(subdir_path)
+        
+if __name__ == "__main__":
+    mp.set_start_method('spawn', force=True)
+
+    parser = argparse.ArgumentParser(description="Co-DETR Sahi Object Detection")
+    
+    parser.add_argument("--images_dir", type=str, required=True, 
+                        default='/home/ubuntu/workspace/datasets/ab_car_validation_germany/2w_misses', help="Directory containing images")
+    parser.add_argument("--output_dir", type=str, required=True, 
+                        default= '/home/ubuntu/workspace/results/ab_car_validation_germany/riders/Co-DETR/2w_misses_codetr', help="Output directory")
+    parser.add_argument("--config_path", type=str, default='./projects/configs/co_dino/co_dino_5scale_lsj_swin_large_3x_coco.py', help="Path to config file")
+    parser.add_argument("--weights_path", type=str, default='./weights/co_dino_5scale_lsj_swin_large_3x_coco.pth', help="Path to weights file")
+    parser.add_argument("--conf_thresh", type=float, default=0.1, help="Confidence threshold")
+    parser.add_argument("--image_size", type=int, default=3840, help="Image size")
+    parser.add_argument("--slice_height", type=int, default=1280, help="Slice height")
+    parser.add_argument("--slice_width", type=int, default=1280, help="Slice width")
+    parser.add_argument("--overlap_height_ratio", type=float, default=0.1, help="Overlap height ratio")
+    parser.add_argument("--overlap_width_ratio", type=float, default=0.1, help="Overlap width ratio")
+    
+    args = parser.parse_args()
+    
+    if not os.path.isfile(args.weights_path):
+        url = 'https://drive.google.com/uc?id=16GTypE-VpvWOPxbK0OxKOj0twvA5dq0L'
+        output = './weights/co_dino_5scale_lsj_swin_large_3x_coco.pth'
+        gdown.download(url, output, quiet=False)
+        
+    if torch.cuda.device_count() > 1:
+        distribute_prediction_on_gpus(args)
+    else:
+        run_on_single_gpu_with_results(torch.device("cuda:0"), [args.images_dir], args, {})
